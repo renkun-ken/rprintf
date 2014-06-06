c02897ffde85673d6e5bc3558274196587e56195
Kun Ren <renkun@outlook.com>
2014-06-06 19:14:46 +0800

    add docs to functions

--- R/rprintn.R
+++ R/rprintn.R
@@ -1,4 +1,26 @@
+#' Build a character vector or list with number-based
+#' string formatting
+#'
+#' The \code{rprintn} function applies number-based formatter to
+#' transform the given character vector to specific texts with
+#' numbers replaced by a given set of values in correct
+#' formats.
+#'
+#' @param x The character vector or list to be transformed
+#' @param ... The arguments that specify the set of values to be
+#'   placed
 #' @export
+#' @examples
+#' \dontrun{
+#'
+#' # Format a single-entry character vector with numbering mechanism
+#' rprintf("Hello, {1}", "world")
+#' rprintf("{1} ({2} years old)","Ken",24)
+#' rprintf("He is {1} but has a height of {2:.2f}cm",18,190)
+#' rprintf("{1}, {2:.1f}, {3:+.2f}, {2}, {1:.0f}",1.56,2.34,3.78)
+#' rprintf("{2},{1}","x","y")
+#' }
+#'
 rprintn <- function(x,...) {
   args <- list(...)
   x <- stringr::str_replace_all(x,"%","%%")
@@ -8,4 +30,3 @@ rprintn <- function(x,...) {
   pass2 <- stringr::str_replace_all(pass1,stringr::perl("(?<!\\{)\\{(\\d+)\\}(?!\\})"),"%s")
   do.call(sprintf,c(list(pass2),args[xss]))
 }
-
--- R/rprintv.R
+++ R/rprintv.R
@@ -1,10 +1,31 @@
+#' Build a character vector or list with variable-based
+#' string formatting
+#'
+#' The \code{rprintv} function applies variable-based formatter to
+#' transform the given character vector to specific texts with
+#' named variables replaced by a given set of values in correct
+#' formats.
+#'
+#' @param x The character vector or list to be transformed
+#' @param ... The arguments that specify the set of values to be
+#'   placed
 #' @export
+#' @examples
+#' \dontrun{
+#'
+#' # Format a single-entry character vector with variable mechanism
+#' rprintf("Hello, $name", name="world")
+#' rprintf("$name ($age years old)",name="Ken",age=24)
+#' rprintf("He is $age but has a height of $height:.2fcm",age=18,height=190)
+#' rprintf("$a, $b:.1f, $c:+.2f, $b, $a:.0f",a=1.56,b=2.34,c=3.78)
+#' }
+#'
 rprintv <- function(x,...) {
   args <- list(...)
   x <- stringr::str_replace_all(x,"%","%%")
   xs <- unlist(stringr::str_extract_all(x,stringr::perl("(?<!\\$)\\$\\w+(:[\\s\\+\\-\\#\\.\\d]*\\w)?")))
   xss <- stringr::str_replace_all(xs,stringr::perl("(?<!\\$)\\$(\\w+)(:[\\s\\+\\-\\#\\.\\d]*\\w)?"),"\\1")
-  pass1 <- stringr::str_replace_all(x,stringr::perl("(?<!\\$)\\$(\\w+):([\\s\\+\\-\\#\\.\\d]*\\w)?"),"%\\2")
+  pass1 <- stringr::str_replace_all(x,stringr::perl("(?<!\\$)\\$(\\w+):(?!\\$)([\\s\\+\\-\\#\\.\\d]*\\w)?"),"%\\2")
   pass2 <- stringr::str_replace_all(pass1,stringr::perl("(?<!\\$)\\$(\\w+)"),"%s")
   do.call(sprintf,c(list(pass2),args[xss]))
 }
