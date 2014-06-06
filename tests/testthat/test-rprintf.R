63633b8c4848acde8301ba18de236dbc3b59adce
Kun Ren <renkun@outlook.com>
2014-06-06 19:57:17 +0800

    add tests

--- tests/testthat/test-rprintf.R
+++ tests/testthat/test-rprintf.R
@@ -1,5 +1,9 @@
-context("simple formatting")
+context("rprintf: general formatting")
 
-test_that("simple formatting works as expected", {
-
+test_that("rprintf works as expected", {
+  expect_equivalent(rprintf("%s","a"),"a")
+  expect_equivalent(rprintf("$name:s",name="a"),"a")
+  expect_equivalent(rprintf("{1:s}","a"),"a")
+  expect_equivalent(rprintf("$name",name="a"),"a")
+  expect_equivalent(rprintf("{1}","a"),"a")
 })
---
+++ tests/testthat/test-rprintv.R
@@ -0,0 +1,12 @@
+context("rprintv: variable-based formatting")
+
+test_that("rprintv works as expected", {
+  expect_equivalent(rprintv("$name:s",name="a"),"a")
+  expect_equivalent(rprintv("$num:d",num=10),"10")
+  expect_equivalent(rprintv("$pi:.2f",pi=pi),"3.14")
+  expect_equivalent(rprintv("$pi:+.2f",pi=pi),"+3.14")
+  expect_equivalent(rprintv("$pi: .2f",pi=pi)," 3.14")
+  expect_equivalent(rprintv("$name1:s,$name",name="a",name1="b"),"b,a")
+  expect_equivalent(rprintv("$a,$b:s,$c:.2f,$b,$a",a="a",b="b",c=1.4),"a,b,1.40,b,a")
+  expect_equivalent(rprintv("$$name",name="a"),"$name")
+})
